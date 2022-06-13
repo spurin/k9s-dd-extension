4b3914807f8dba9982576bcacc4b07a2fd2f8af7
Maahi B <tukobadnyanoba.28@gmail.com>
2022-06-13 12:19:45 +0530

    interim commit

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,6 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import {DockerMuiThemeProvider} from '@docker/docker-mui-theme';
 import {createDockerDesktopClient} from '@docker/extension-api-client';
 import "./App.css";
+import {createLoftToolKitContainer, createVolume} from "./docker-client";
 
 const client = createDockerDesktopClient();
 
@@ -15,99 +16,18 @@ function App() {
     const [response, setResponse] = React.useState("");
     const ddClient = useDockerDesktopClient();
 
-    async function createVolume() {
-        await ddClient.docker.cli.exec("volume", ["create", "loft-toolkit"], {
-            stream: {
-                onOutput(data) {
-                    if (data.stdout) {
-                        console.log("[volume created] : ", data.stdout);
-                    } else {
-                        console.error("[volume creation failed] : ", data.stderr);
-                    }
-                }, onError(error) {
-                    console.error("[volume creation failed] : ", error);
-                },
-            },
-        });
-    }
-
-    async function createLoftToolKitContainer() {
-        await ddClient.docker.cli.exec("run", ["-d", "--name", "loft-toolkit", "-v", "loft-toolkit:/root/.kube", "loft-toolkit:0.0.2"], {
-            stream: {
-                onOutput(data) {
-                    if (data.stdout) {
-                        console.log("[container created] : ", data.stdout);
-                    } else {
-                        console.error("[container creation failed] : ", data.stderr);
-                    }
-                }, onError(error) {
-                    console.error("[container creation failed] : ", error);
-                }
-            },
-        });
-    }
-
-    async function resetK8sConnection() {
-        await deleteLoftToolKitContainer()
-        await deleteLoftToolKitVolume()
-    }
-
-    async function isLoftToolKitPodRunning() {
-        return true
-    }
-
-    async function isLoftToolKitVolumeAttached() {
-        return true
-    }
-
-    async function isK8sConnectionActive() {
-        return await isLoftToolKitPodRunning() && await isLoftToolKitVolumeAttached()
-    }
-
-    async function deleteLoftToolKitVolume() {
-        await ddClient.docker.cli.exec("volume", ["rm", "loft-toolkit", "-f"], {
-            stream: {
-                onOutput(data) {
-                    if (data.stdout) {
-                        console.log("[volume deleted] : ", data.stdout);
-                    } else {
-                        console.error("[volume deletion failed] : ", data.stderr);
-                    }
-                }, onError(error) {
-                    console.error("[volume deletion failed] : ", error);
-                }
-            },
-        });
-    }
-
-    async function deleteLoftToolKitContainer() {
-        await ddClient.docker.cli.exec("container", ["rm", "loft-toolkit", "-f"], {
-            stream: {
-                onOutput(data) {
-                    if (data.stdout) {
-                        console.log("[container deleted] : ", data.stdout);
-                    } else {
-                        console.error("[container deletion failed] : ", data.stderr);
-                    }
-                }, onError(error) {
-                    console.error("[container deletion failed] : ", error);
-                }
-            },
-        });
-    }
-
     const get = async () => {
-        await createVolume();
-        await createLoftToolKitContainer();
+        await createVolume(ddClient);
+        await createLoftToolKitContainer(ddClient);
         const result1 = await ddClient.extension.vm.service.get("/hello");
         console.log(result1)
         setResponse(JSON.stringify(result1));
     };
 
-    const createVCluster = async () => {
-        const result = await ddClient.extension.vm.service.post("/vClusters");
-        setResponse(JSON.stringify(result));
-    };
+    // const createVCluster = async () => {
+    //     const result = await ddClient.extension.vm.service.post("/vClusters");
+    //     setResponse(JSON.stringify(result));
+    // };
     //
     // const deleteVCluster = async () => {
     //     const result = await ddClient.extension.vm.service.delete("/vcluster/cluster-1");
