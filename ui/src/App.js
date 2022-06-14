a93246a7720840d511aba0b7d4d95918d294b4ff
Maahi B <tukobadnyanoba.28@gmail.com>
2022-06-14 20:40:48 +0530

    added link

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,7 +3,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import {DockerMuiThemeProvider} from "@docker/docker-mui-theme";
 import VCluster from "./vcluster/Vcluster";
 import Branding from "./branding/Branding";
-import {Box, Stack} from "@mui/material";
+import {Box, Link, Stack} from "@mui/material";
 
 export default function App() {
     const [show, setShow] = React.useState(false);
@@ -21,19 +21,21 @@ export default function App() {
 
     return (<DockerMuiThemeProvider>
         <CssBaseline/>
-        <Stack direction="column" spacing={2}>
-            <Box
-                component="img"
-                sx={{
-                    alignSelf: "center",
-                    height: 100,
-                    width: 300,
-                    maxHeight: {xs: 150, md: 350},
-                    maxWidth: {xs: 150, md: 350},
-                }}
-                src="https://d33wubrfki0l68.cloudfront.net/0ba1c19e054b699c5642c768cdb3145123611f8b/0d4b0/images/vcluster-logo.svg"
-            />
-        </Stack>
+        <Link href="/">
+            <Stack direction="column" spacing={2}>
+                <Box
+                    component="img"
+                    sx={{
+                        alignSelf: "center",
+                        height: 100,
+                        width: 300,
+                        maxHeight: {xs: 150, md: 350},
+                        maxWidth: {xs: 150, md: 350},
+                    }}
+                    src="https://d33wubrfki0l68.cloudfront.net/0ba1c19e054b699c5642c768cdb3145123611f8b/0d4b0/images/vcluster-logo.svg"
+                />
+            </Stack>
+        </Link>
         {component}
     </DockerMuiThemeProvider>);
 }
